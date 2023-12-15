0a19a97355f0130f653ca9a806a270ec52f3694b
carlos6199 <carlos_fig14@hotmail.com>
2023-12-15 16:25:25 -0500

    updated style and added new project

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,30 @@
 const Header = () => {
     
+    const backgroundImage = {
+       'position':'relative',
+        'backgroundImage':"url('https://images.unsplash.com/photo-1517694712202-14dd9538aa97?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')",
+        'backgroundPosition':'center',
+        'backgroundSize':'cover',
+        'opacity':'0.5',
+        'backgroundColor':'black'
+    }
+
+    const quote = {
+        'position':'absolute',
+        'top':'40%',
+        'left':'20%',
+    }
 
 
     return (
-        <div className='header' class=' bg-sky-900' >
-            <div class='text-white text-center text-xl w-80 py-40 pl-20'>A Full Stack Developer on a journey to inspire the world!</div>
+        <div class='bg-black relative'>
+            <div className='header' class=' h-screen flex justify-center items-center' style={backgroundImage}>
+            
+            </div>
+            <div class='text-white text-center text-3xl w-1/2 h-full font-semibold ' style={quote}>A Full Stack Developer on a journey to inspire the world!</div>
             
         </div>
+        
     )
 }
 
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,26 +1,35 @@
 import {Link} from 'react-router-dom'
 
 const Projects = () => {
+
+    const projectStyle = 'h-60 w-40 py-20 border-4 border-zinc-200  text-center rounded-2xl bg-zinc-400 hover:bg-blue-600 font-bold text-white'
+
     return(
         <div className="projects-container" >
 
             <h2 class='text-center text-2xl'>Projects</h2>
-            <div className="projects" class='flex justify-around w-1/2 mx-auto my-10'>
+            <div className="projects" class='flex justify-around w-3/4 mx-auto my-10'>
                 <Link to='https://regal-tanuki-9c1a67.netlify.app' >
-                    <div className="project" class='h-60 w-40 px-5 py-20 border-2 boder-black border-blue-300 text-center rounded-2xl  hover:bg-blue-300'>
+                    <div className="project" class={projectStyle}>
                         <h3>Star Wars Trivia</h3>
                     </div>
                 </Link>
                 
                 <Link to='https://creative-strudel-0cfd0a.netlify.app' >
-                    <div className="project" class='h-60 w-40 px-5 py-20 border-2 boder-black border-blue-300 text-center rounded-2xl  hover:bg-blue-300'>
+                    <div className="project" class={projectStyle}>
                         <h3>Hangman</h3>
                     </div>
                 </Link>
                 
                 <Link to='https://starlit-cupcake-b53836.netlify.app' >
-                    <div className="project" class='h-60 w-40 px-5 py-20 border-2 boder-black border-blue-300 text-center rounded-2xl  hover:bg-blue-300'>  
+                    <div className="project" class={projectStyle}>  
                         <h3>Blog App</h3>
+                    </div>
+                </Link>
+
+                <Link to='https://wonderful-wisp-ad741f.netlify.app/'>
+                    <div className="project" class={projectStyle}>
+                        <h3>Daytracker</h3>
                     </div>
                 </Link>
                 
